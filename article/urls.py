293f093f141e0b9aded24840620741cfde8102aa
vineetjai <vineetj869@gmail.com>
2017-08-09 16:12:41 +0530

    language change is set

--- article/urls.py
+++ article/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import *
-from article.views import articles
-from article.views import article
-from article.views import hello
-from article.views import hello_template
-from article.views import HelloTemplate
-from article.views import hello_template_simple
-
+from article.views import *
 from django.contrib import admin
 admin.autodiscover()
 urlpatterns=[
 	url(r'^admin/', admin.site.urls),
 	url(r'^articles/all/$',articles,name='articles'),
 	url(r'^article/get/(?P<article_id>\d+)/$',article,name='article'),
+	url(r'^articles/language/(?P<language>[a-z\-]+)/$',language,name='language'),
     url(r'^hello/$',hello,name='hello'),
     url(r'^hello_class_view/$',HelloTemplate.as_view()),
     url(r'^hello_template/$',hello_template,name='hello_template'),
--- article/views.py
+++ article/views.py
@@ -11,7 +11,6 @@ from django.shortcuts import render_to_response
 from django.views.generic.base import TemplateView
 from article.models import Article
 
-
 def hello(request):
 	name="Vineet"
 	html="<html><body>Hi %s, this seems to be working!</body></html>" % name
@@ -32,6 +31,17 @@ class HelloTemplate(TemplateView):
 		return context
 
 def articles(request):
-	return render_to_response('articles.html',{'articles':Article.objects.all()})
+	language ="en-gb"
+	session_language ='en-gb'
+	if 'lang' in request.COOKIES:
+		language =request.COOKIES['lang']
+	if 'lang' in request.session:
+		session_language =request.session['lang']
+	return render_to_response('articles.html',{'articles':Article.objects.all(),'language':language,'session_language':session_language})
 def article(request,article_id=1):
 	return render_to_response('article.html',{'article':Article.objects.get(id=article_id)})
+def language(request,language='en-gb'):
+	response =HttpResponse("setting language to %s"%language)
+	response.set_cookie('lang',language)
+	request.session['lang']=language
+	return response
